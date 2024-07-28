9d685465b50e0027c47d5753bcf26c7f7bdbc839
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-07-28 21:54:15 +0000

    arc not working

--- benchmarks/create_bm_raytrace_slipcover.py
+++ benchmarks/create_bm_raytrace_slipcover.py
@@ -4,8 +4,8 @@ from pathlib import Path
 
 
 def main():
-    b = json.load(Path("cov_data/slipcover_raytrace_black_orig.json").open("r"))
-    a = CoverageData("cov_data/.coverage_raytrace_black_slipcover")
+    b = json.load(Path("cov_data/slipcover_raytrace_black_branch_orig.json").open("r"))
+    a = CoverageData("cov_data/.coverage_raytrace_black_slipcover_branch")
     a.erase()
     a.write()
 
@@ -15,9 +15,38 @@ def main():
         str(Path().joinpath(file).resolve()): data["executed_lines"]
         for file, data in b["files"].items()
     }
-
     print("line_data: ", line_data)
     a.add_lines(line_data)
+
+    arc_data = {
+        str(Path().joinpath(file).resolve()): (
+            (l1, l2) for l1, l2 in data["executed_branches"]
+        )
+        for file, data in b["files"].items()
+    }
+    arc_data3 = {
+        str(Path().joinpath(file).resolve()): [
+            (item[0], item[1]) for item in data["executed_branches"]
+        ]
+        for file, data in b["files"].items()
+    }
+
+    arc_data2 = {}
+    for file, data in b["files"].items():
+        file2 = str(Path().joinpath(file).resolve())
+        for item in data["executed_branches"]:
+            print("item: ", item)
+            arc = [(l1, l2) for l1, l2 in item]
+            arc_data2[file2].append(arc)
+
+        arc = [(item[0], item[1]) for item in data["executed_branches"]]
+        arc_data2[file2] = arc
+        # arc_data2[file2] = data
+
+    print("arc_data: ", arc_data)
+    print("arc_data: ", arc_data2)
+    print("arc_data: ", arc_data3)
+    a.add_lines(arc_data3)
     a.write()
 
 
